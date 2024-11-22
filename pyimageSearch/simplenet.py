3f8a42284588efdcb02326090fc5917a70458117
Harmandeepsinghteja <harmandeepsinghteja@gmail.com>
2024-11-22 11:09:39 -0700

    Importing libraries

---
+++ pyimageSearch/simplenet.py
@@ -0,0 +1,8 @@
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D
+from tensorflow.keras.layers import Activation
+from tensorflow.keras.layers import Flatten
+from tensorflow.keras.layers import Dense
+from tensorkflow.keras import MaxPooling2D
+from tensorflow.keras import backend as K
+from tensorflow.keras import Dropout
